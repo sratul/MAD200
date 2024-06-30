1d4e5546c8bd5d94cc4f8d6ffec5047785a63f3e
sratul <saiful_ratul@outlook.com>
2024-06-30 11:32:23 -0400

    Scaffold Added

--- class_lectures/lib/main.dart
+++ class_lectures/lib/main.dart
@@ -6,9 +6,23 @@ void main() {
   // which is a built in widget
   // wrapper using Google Design Style
 
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
     // const improves performance because flutter does
     // not need to check each time for a change
-    home: Text("Hello"), // default screen of app
+    // appBars cannot be a constant anymore so we have to remove the const
+    home: Scaffold(
+      // each screen will have its own scaffold
+      // appBar argument with an appBar widget
+      appBar: AppBar(
+        // instead we can add the const here whenever we encounter blue
+        // squigly lines
+        // helps with performance but we may have to remove them once again
+        title: const Text("Hello World"), // arguments end with comma
+        backgroundColor: Colors.blueGrey[500],
+        centerTitle: true,
+      ),
+      body: const Text("Hello to All of you!!!!"),
+    ), // default screen of app
   ));
 }
+// needed an ancestor widget for proper design
