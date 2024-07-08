de39070718086d24422ec1cfb0d9fd3712cff733
sratul <saiful_ratul@outlook.com>
2024-07-08 11:15:08 -0400

    added appbars for all pages

--- class_lectures/lib/main.dart
+++ class_lectures/lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:class_lectures/pages/choose_location.dart';
 import 'package:class_lectures/pages/home.dart';
+import 'package:class_lectures/pages/loading.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Home(),
+    initialRoute: '/home',
+    routes: {
+      '/': (context) => Loading(),
+      '/home': (context) => Home(),
+      '/location': (context) => ChooseLocation(),
+    },
   ));
 }
--- class_lectures/lib/pages/home.dart
+++ class_lectures/lib/pages/home.dart
@@ -14,7 +14,19 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       // Since we are not using any AppBar widget
-      body: SafeArea(child: Text("Home Screen")),
+      body: SafeArea(
+          child: Column(
+        children: [
+          FilledButton.icon(
+            onPressed: () {
+              // the current screen is under the new pushedh screen
+              Navigator.pushNamed(context, '/location');
+            },
+            icon: Icon(Icons.edit_location),
+            label: Text('Edit Location'),
+          )
+        ],
+      )),
     );
   }
 }
