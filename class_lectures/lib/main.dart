6400f95e59d2438d3d3591db203628bcf0d7e3ed
sratul <saiful_ratul@outlook.com>
2024-07-15 17:17:00 -0400

    initial

--- class_lectures/lib/main.dart
+++ class_lectures/lib/main.dart
@@ -3,3 +3,52 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp());
 }
+
+class FormValidationApp extends StatelessWidget {
+  const FormValidationApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormState>();
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Form Validation Example"),
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(20),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextFormField(
+                    decoration: InputDecoration(labelText: 'Name'),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your name';
+                      }
+                      return null;
+                    }),
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Email'),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your email';
+                    }
+                    if (!value.contains('@')) {
+                      return 'Please enter valid email';
+                    }
+
+                    return null;
+                  },
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
