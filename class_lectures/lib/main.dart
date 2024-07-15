17e3dbbbbc4f87f4bfaa923d493fd9884568fc43
sratul <saiful_ratul@outlook.com>
2024-07-15 16:00:35 -0400

    tap detector

--- class_lectures/lib/main.dart
+++ class_lectures/lib/main.dart
@@ -1,5 +1,37 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp());
+  runApp(TapDetectorApp());
+}
+
+class TapDetectorApp extends StatefulWidget {
+  const TapDetectorApp({super.key});
+
+  @override
+  State<TapDetectorApp> createState() => _TapDetectorAppState();
+}
+
+class _TapDetectorAppState extends State<TapDetectorApp> {
+  int tapCount = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Tap Detector'),
+        ),
+        body: Center(
+          child: GestureDetector(
+            onTap: () {
+              setState(() {
+                tapCount++;
+              });
+            },
+            child: Text('Tap Count: $tapCount'),
+          ),
+        ),
+      ),
+    );
+  }
 }
